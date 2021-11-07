afee0cbf0e6cad8de8181470a419eec8df912cec
Armando Arciniega <armandoarciniega@ciencias.unam.mx>
2021-11-07 00:01:12 -0600

    nuevos cambios en la pagina de productos, aun falta terminarlo

---
+++ JS/main.js
@@ -0,0 +1,48 @@
+/** Función addItem agrega al html un card con **/
+
+function addItem(item){
+    const itemHTML =       
+            '   <img src="'+item.img+'" class="card-img-top" alt="descripcion de la imagen"> \n'+
+            '   <div class="card-body">\n'+
+            '   <h5 class="card-title">'+item.name+'</h5>\n'+
+            '   <p class="card-text">'+item.description +'</p>\n'+
+            '   <p class="card-text">'+item.price +'</p>\n'+
+            '   </div>\n';
+
+
+
+//***********************     itemsContainer identifica el listado de elementos del html con "list-items" ylos agrega al html       *************************/
+    const itemsContainer = document.getElementById("list-items");    
+    itemsContainer.innerHTML += itemHTML;
+}
+
+//*****************    Listado de 10 elementos en formato json      ********************/
+/****  json: Nombre del articulo, imagen en formato jpg, descripción del artículo, (precio)  ****/
+
+
+addItem({'name':'playera 1',
+    'img':'./img/Index/amorAlaMexicana.jpg',
+    'description':'Playera mujer', 
+    'price' : '$50.00'});
+
+
+addItem({'name':'playera 3',
+    'img':'./img/Index/dosDeTresCaidas.jpg',
+    'description':'Playera de hombre', 
+    'price' : '$65.00'});
+
+addItem({'name':'playera 4',
+    'img':'./img/Index/amorAlaMexicana.jpg',
+    'description':'Playera mujer', 
+    'price' : '$50.00'});
+
+
+addItem({'name':'playera 2',
+    'img':'./img/Index/dosDeTresCaidas.jpg',
+    'description':'Playera de hombre', 
+    'price' : '$65.00'});
+
+
+
+
+    
