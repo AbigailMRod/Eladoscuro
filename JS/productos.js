da03bf6f208343dc1f4f2fee4a718d9d7561a541
Armando Arciniega <armandoarciniega@ciencias.unam.mx>
2021-11-11 13:33:25 -0600

    version final de pagina productos

--- JS/productos.js
+++ JS/productos.js
@@ -1,14 +1,14 @@
 //** Funcion para playeras**/
 function addItem(item){ 
     const itemHTML =  
-            '   <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">\n'+
+            '   <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">\n'+ 
             '   <div class="card">\n' +
-            '   <img src="'+item.img+'" class="card-img-top" style="height: 350%;" style="width:180%;"  alt="  "> \n'+
+            '   <img src="'+item.img+'" class="card-img-top" alt="  "> \n'+
             '   <div class="card-body">\n'+
             '   <h5 class="card-title">'+item.model+'</h5>\n'+
             '   <p class="card-text">'+item.description +'</p>\n'+
-            '   <p class="card-text">'+item.price +'</p>\n'+
-            '   <p class="card-text">'+item.size +'</p>\n'+
+            '   <p class="card-text">'+item.price +' MX </p>\n'+
+            '   <p class="card-text"> Tallas: XS, S, M, L XL </p>\n'+
             '   <button type="button" class="btn btn-outline-dark">Comprar</button>\n'+
             '   </div>\n'+
             '   </div>\n'+
@@ -20,13 +20,15 @@ function addItem(item){
 //** Funcion para stickers**/
 function addItem2(item){ 
     const itemHTML =  
+            '   <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">\n'+ 
             '   <div class="card">\n' +
             '   <img src="'+item.img+'" class="card-img-top" alt="  "> \n'+
             '   <div class="card-body">\n'+
             '   <h5 class="card-title">'+item.name+'</h5>\n'+
             '   <p class="card-text">'+item.description +'</p>\n'+
-            '   <p class="card-text">'+item.price +'</p>\n'+
+            '   <p class="card-text">'+item.price +' MX </p>\n'+
             '   <button type="button" class="btn btn-outline-dark">Comprar</button>\n'+
+            '   </div>\n'+
             '   </div>\n'+
             '   </div>';
     const itemsContainer = document.getElementById("list-items2");    
@@ -36,13 +38,15 @@ function addItem2(item){
 //** Funcion para posters**/
 function addItem3(item){ 
     const itemHTML =  
+            '   <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">\n'+ 
             '   <div class="card">\n' +
             '   <img src="'+item.img+'" class="card-img-top" alt="  "> \n'+
             '   <div class="card-body">\n'+
             '   <h5 class="card-title">'+item.name+'</h5>\n'+
             '   <p class="card-text">'+item.description +'</p>\n'+
-            '   <p class="card-text">'+item.price +'</p>\n'+
+            '   <p class="card-text">'+item.price +' MX </p>\n'+
             '   <button type="button" class="btn btn-outline-dark">Comprar</button>\n'+
+            '   </div>\n'+
             '   </div>\n'+
             '   </div>';
     const itemsContainer = document.getElementById("list-items3");    
@@ -53,44 +57,34 @@ function addItem3(item){
 
 
 
-//********************************LISTADO DE ARTICULOS*************************************//
+//******************************** LISTADO DE ARTICULOS (FORMATO JSON) **************************************//
 
 //********* PLAYERAS  *******//
-
 addItem({'model':'Amor a la mexicana',
     'img':'./../img/Index/playera_1.jpg',         
-    'description':'Sudadera para hombre', 
-    'size':'Talla: XS, S, M, L, XL',
+    'description':'Sudadera para hombre',
     'price' : '$800.00'});
-
 
 addItem({'model':'Amor a la mexicana',
     'img':'./../img/Index/playera_2.jpg', 
-    'description':'Playera para mujer', 
-    'size':'Talla: XS, S, M, L, XL',
+    'description':'Playera para mujer',
     'price' : '$400.00'});
 
 addItem({'model':'Lucha libre',
     'img':'./../img/Index/playera_3.jpg',
     'description':'Playera para hombre',
-    'size':'Talla: XS, S, M, L, XL', 
     'price' : '$400.00'});
-
 
 addItem({'model':'Lucha libre',
     'img':'./../img/Index/playera_4.jpg',
-    'description':'Playera para mujer', 
-    'size':'Talla: XS, S, M, L, XL',
+    'description':'Playera para mujer',
     'price' : '$400.00'});
 
-    
 //********* STICKERS  *******//
-
 addItem2({'name':'Logo',
     'img':'./../img/Index/sticker_1.jpg',        
     'description':'Juego de stickers 1', 
     'price' : '$100.00'});
-
 
 addItem2({'name':'Logo + amor ',
     'img':'./../img/Index/sticker_2.jpg', 
@@ -102,19 +96,16 @@ addItem2({'name':'Lucha libre',
     'description':'Juego de stickers 3', 
     'price' : '$100.00'});
 
-
 addItem2({'name':'Amor a la mexicana',
     'img':'./../img/Index/sticker_4.jpg',
     'description':'Juego de stickers 4', 
     'price' : '$100.00'}); 
  
 //********* POSTERS  *******//
-
 addItem3({'name':'Lucha libre',
     'img':'./../img/Index/poster_1.jpg',        
     'description':'Poster tamaño carta', 
     'price' : '$50.00'});
-
 
 addItem3({'name':'Lucha libre + amor',
     'img':'./../img/Index/poster_2.jpg', 
@@ -125,7 +116,6 @@ addItem3({'name':'Lucha libre',
     'img':'./../img/Index/poster_3.jpg',
     'description':'Poster decorativo', 
     'price' : '$75.00'});
-
 
 addItem3({'name':'Amor a la mexicana',
     'img':'./../img/Index/poster_4.jpg',
