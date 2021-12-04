844ba5f08819e812b868cc93bac67c9dac106964
Christian Neri <neri.christian98@gmail.com>
2021-12-03 22:32:09 -0600

    Se implementó UserRepository que conecta con la base de datos del usuario

---
+++ Proyecto Spring Boot/ecommerce/src/test/java/com/eladoscuro/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eladoscuro.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
