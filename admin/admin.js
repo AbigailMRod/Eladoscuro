06b2c9d9f2b7d73982fac5520ed168bb1319f643
cristian-iram-monarres-mtz <cristian.monarres.mtz@gmail.com>
2021-12-04 23:59:54 -0600

    se cambió la imagen por defecto de la página admin

--- admin/admin.js
+++ admin/admin.js
@@ -1,68 +1,64 @@
-function inicio(){    
-        window.location= "./../pages/productos.html";
-        }// función para regresar a la página de inicio   
-    
-    /********************************  para añadir elementos al JSON******************************/ 
-    function saveProduct(e){
-        e.preventDefault();
-        let nombre= document.getElementById("nombre").value;
-        let descripcion= document.getElementById("descripcion").value;
-        let precio= document.getElementById("precio").value;
-        let image = document.getElementById("product_img").src; /* ************************** */
-        let category= document.getElementById("categoria").options [document.getElementById("categoria").selectedIndex].value;
-        
-    
-        let newProduct={
-                "model":nombre,
-                "price": precio,
-                "description": descripcion,
-                "img": image,
-                "category":category
-        };//new Product
-    
-        let valueInLocalStorage=  window.localStorage.getItem("localProduct");
-        let storeList;
-        if(valueInLocalStorage){
-           // 1. Traer lo que tengas en el localStorage
-           // 2. Convertirlo de json a un arreglo
-           // 3. hacer push del nuevo objeto
-           // 4. Volver a arreglo a json
-           // 5. Actualizar el item  
-        storeList= JSON.parse(valueInLocalStorage)
+function inicio() {
+    window.location = "./../pages/productos.html";
+} // función para regresar a la página de inicio   
+
+/********************************  para añadir elementos al JSON******************************/
+function saveProduct(e) {
+    e.preventDefault();
+    let nombre = document.getElementById("nombre").value;
+    let descripcion = document.getElementById("descripcion").value;
+    let precio = document.getElementById("precio").value;
+    let image = document.getElementById("product_img").src; /* ************************** */
+    let category = document.getElementById("categoria").options[document.getElementById("categoria").selectedIndex].value;
+
+
+    let newProduct = {
+        "model": nombre,
+        "price": precio,
+        "description": descripcion,
+        "img": image,
+        "category": category
+    }; //new Product
+
+    let valueInLocalStorage = window.localStorage.getItem("localProduct");
+    let storeList;
+    if (valueInLocalStorage) {
+        // 1. Traer lo que tengas en el localStorage
+        // 2. Convertirlo de json a un arreglo
+        // 3. hacer push del nuevo objeto
+        // 4. Volver a arreglo a json
+        // 5. Actualizar el item  
+        storeList = JSON.parse(valueInLocalStorage)
         storeList.push(newProduct);
         console.log(storeList)
-        window.localStorage.setItem("localProduct",JSON.stringify(storeList));
-        }
-    }// save Product
-    
-    // aseguramos que se ejecute el js despues haber cargado todos los recursos
-    //externos
-    
-    function cloud (){
+        window.localStorage.setItem("localProduct", JSON.stringify(storeList));
+    }
+} // save Product
+
+// aseguramos que se ejecute el js despues haber cargado todos los recursos
+//externos
+
+function cloud() {
     let image = document.getElementById("product_img");
-        let myWidget = cloudinary.createUploadWidget({
-            cloudName: 'dtcyppikq',
-            uploadPreset: 'wpicwpay'
-        }, (error, result) => {
-            if (!error && result && result.event === 'success'){
-                // console.log('Imagen subida con éxito', result.info);
-                image.src = result.info.secure_url;
-                // console.log(result.info.secure_url);
-            }
-        });
-        document.querySelector('#upload_widget').addEventListener("click", function(){
-            myWidget.open();
-    
-        }, false);
-    }// para guardar las imágenes en Cloudinary
-    
-    
-    window.onload = function(){
+    let myWidget = cloudinary.createUploadWidget({
+        cloudName: 'dtcyppikq',
+        uploadPreset: 'wpicwpay'
+    }, (error, result) => {
+        if (!error && result && result.event === 'success') {
+            // console.log('Imagen subida con éxito', result.info);
+            image.src = result.info.secure_url;
+            // console.log(result.info.secure_url);
+        }
+    });
+    document.querySelector('#upload_widget').addEventListener("click", function() {
+        myWidget.open();
+
+    }, false);
+} // para guardar las imágenes en Cloudinary
+
+
+window.onload = function() {
         console.log("cargo")
         document.getElementById('formulario').addEventListener('submit', saveProduct);
         cloud();
-    }//window
-
-
-
-
+    } //window
